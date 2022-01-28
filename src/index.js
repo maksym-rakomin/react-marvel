04b1fe63b65b3e0be8ffdc93589153c25b2ebfff
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-01-28 09:41:42 +0200

    implement request random char

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelService from "./services/MarvelService";
 import './style/style.scss';
-
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters().then(res => console.log(1, res))
-marvelService.getCharacter(1010914).then(res => console.log(2, res))
 
 ReactDOM.render(
   <React.StrictMode>
